79b66c27d57bf4b6a647d40f0ea66973a5f08999
Rhyan Reyes <rhyan.reyes@live.com>
2018-10-01 17:06:19 -0700

    New code components

---
+++ curbmap-participate/src/Participate/ParticipateApp.js
@@ -0,0 +1,27 @@
+import react, { Component } from "react";
+
+class ParticipateApp extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Curbmap Participate App</h1>
+        <div className="container">
+          <div className="item">
+            <p>
+              <i>Picture area</i>
+            </p>
+          </div>
+          <div className="item">
+            <p>
+              <i>Times area (?)</i>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ParticipateApp;
